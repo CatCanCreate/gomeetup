30765596cc78ed8d0ce70e4895ca3654e8cb2492
Michail Shatalow <misha.shatalow@gmail.com>
2020-04-25 12:00:00 +0200

    Add config file

--- cmd/gomeetup/main.go
+++ cmd/gomeetup/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"github.com/CatCanCreate/gomeetup/internal/config"
 	"github.com/CatCanCreate/gomeetup/internal/handler"
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	h := handler.NewHandler()
 
 	r := chi.NewRouter()
@@ -15,7 +23,7 @@ func main() {
 	r.Get("/hello", h.Hello)
 
 	log.Println("server start on http://localhost:8080")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatalln(err)
 
 }
